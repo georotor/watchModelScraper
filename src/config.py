70f2aad4e7ebc557d0d195fc98e7889179a7ac4b
Arthur <lakiza@gmail.com>
2023-07-03 15:57:18 +0300

    Downloads with threads

--- src/config.py
+++ src/config.py
@@ -4,9 +4,11 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     """Настройки приложения."""
 
+    debug: bool = False
     firefox_remote: str = 'http://localhost:4444/wd/hub'
-    watch_model_url: str = 'https://watchmdh.to/models/annacute/'
     downloads_dir: str = 'downloads'
+    downloads_threads: int = 3
+    watch_model_url: str
 
     class Config:
         env_nested_delimiter = '__'
