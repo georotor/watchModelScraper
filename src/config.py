adfc810f31d67cc7c9144821ee4efb8d0da36e58
Arthur <lakiza@gmail.com>
2023-07-03 14:14:10 +0300

    Downloads to Synology

--- src/config.py
+++ src/config.py
@@ -4,9 +4,15 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     """Настройки приложения."""
 
+    debug: bool = False
     firefox_remote: str = 'http://localhost:4444/wd/hub'
-    watch_model_url: str = 'https://watchmdh.to/models/annacute/'
-    downloads_dir: str = 'downloads'
+    watch_model_url: str
+    syno_tasks: int = 3
+    syno_wait_timeout: int = 2
+    syno_url: str
+    syno_port: int = 5000
+    syno_user: str
+    syno_password: str
 
     class Config:
         env_nested_delimiter = '__'
--- src/main.py
+++ src/main.py
@@ -1,24 +1,17 @@
-import os
-
-import requests
-
-from urllib.parse import urlparse
-from urllib3.util.retry import Retry
 from time import sleep
 
-from config import settings
-from requests.adapters import HTTPAdapter
-from requests.sessions import Session
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
-from tqdm import tqdm
+from synology_dsm import SynologyDSM
+
+from config import settings
 
 
-def get_driver(debug: bool = False) -> WebDriver:
-    if debug:
+def get_driver() -> WebDriver:
+    if settings.debug:
         driver = webdriver.Firefox()
         driver.install_addon("ublock_origin-1.50.0.xpi")
         return driver
@@ -36,15 +29,20 @@ def get_driver(debug: bool = False) -> WebDriver:
     return driver
 
 
-def get_video_link(driver: WebDriver, url: str) -> str:
+def get_video_link(url: str) -> str:
+    driver = get_driver()
     driver.get(url)
+
     el = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.CSS_SELECTOR, "video.fp-engine[src]"))
     link = el.get_attribute("src")
+
+    driver.quit()
 
     return link
 
 
-def get_links(driver: WebDriver, url: str) -> list:
+def get_links(url: str) -> list:
+    driver = get_driver()
     links = []
 
     driver.get(url)
@@ -54,8 +52,8 @@ def get_links(driver: WebDriver, url: str) -> list:
         elements = driver.find_elements(By.CSS_SELECTOR, "#list_videos_common_videos_list_items a[href]")
         for item in elements:
             link = item.get_attribute("href")
-            print(f"[>] Find link \"{link}\"")
             links.append(link)
+            print(f"[{len(links)}] Find link \"{link}\"")
 
         try:
             driver.find_element(By.CSS_SELECTOR, "div.pagination-holder li.next a[href]").click()
@@ -64,75 +62,44 @@ def get_links(driver: WebDriver, url: str) -> list:
         except NoSuchElementException:
             break
 
+    driver.quit()
+
     return links
 
 
-def create_session() -> Session:
-    session = requests.Session()
+def syno_wait(syno: SynologyDSM):
+    while True:
+        sleep(settings.syno_wait_timeout)
 
-    retry = Retry(connect=7, backoff_factor=0.5, raise_on_redirect=False, raise_on_status=False)
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
+        syno.download_station.update()
 
-    session.headers.update({
-        'User-Agent': 'Googlebot/2.1 (+http://www.google.com/bot.html)',
-        'Referer': 'https://watchmdh.to/'
-    })
+        tasks = syno.download_station.get_all_tasks()
+        tasks_enabled = 0
 
-    return session
+        for task in tasks:
+            if task.status in ('downloading', 'waiting'):
+                tasks_enabled += 1
 
-
-def get_url_data(url) -> dict:
-    parsed_url = urlparse(url)
-    return {'file_name': os.path.basename(parsed_url.path.rstrip('/')), 'extension': os.path.splitext(parsed_url.path)[1],
-            'hostname': parsed_url.hostname}
-
-
-def download(url, download_path):
-    session = create_session()
-
-    file_name = get_url_data(url)['file_name']
-    final_path = os.path.join(download_path, file_name)
-
-    if os.path.isfile(final_path):
-        print(f"\t[-] Error Downloading \"{file_name}\" exists")
-        return
-
-    try:
-        with session.get(url, stream=True) as r:
-            if r.status_code != 200:
-                print(f"\t[-] Error Downloading \"{file_name}\": {r.status_code}")
-                return
-
-            file_size = int(r.headers.get('content-length', -1))
-            with open(final_path, 'wb') as f:
-                with tqdm(total=file_size, unit='iB', unit_scale=True, desc=file_name, leave=False) as pbar:
-                    for chunk in r.iter_content(chunk_size=8192):
-                        if chunk is not None:
-                            f.write(chunk)
-                            pbar.update(len(chunk))
-
-    except requests.exceptions.ConnectionError:
-        print(f"\t[-] Unable download link {url}")
+        if tasks_enabled < settings.syno_tasks:
+            break
 
 
 def main():
-    driver = get_driver()
+    syno = SynologyDSM(
+        settings.syno_url, settings.syno_port,
+        settings.syno_user, settings.syno_password
+    )
 
     url = settings.watch_model_url
-    links = get_links(driver, url)
+    links = get_links(url)
 
     for link in links:
-        if driver is None:
-            driver = get_driver()
+        syno_wait(syno)
 
-        url = get_video_link(driver, link)
-        driver.quit()
-        driver = None
+        url = get_video_link(link)
 
         print(f"[+] Downloading link {url}")
-        download(url, settings.downloads_dir)
+        syno.download_station.create(url)
 
 
 if __name__ == '__main__':
